e44833b16088a96f023efeb5838ee43a2e6aa559
tgtiburon <tg.tiburon@gmail.com>
2022-03-09 10:16:11 -0500

    card model complete

--- server/models/Enemy.js
+++ server/models/Enemy.js
@@ -1,1 +1,12 @@
 // currentHealth, maxHealth, intents
+const mongoose = require('mongoose');
+const { Schema }   = mongoose;
+
+const cardSchema = new Schema({
+
+
+})
+
+const Card = mongoose.model('Card', cardSchema);
+
+module.exports = Card;
--- server/models/Room.js
+++ server/models/Room.js
@@ -1,1 +1,12 @@
 // id background image enemies
+const mongoose = require('mongoose');
+const { Schema }   = mongoose;
+
+const cardSchema = new Schema({
+
+
+})
+
+const Card = mongoose.model('Card', cardSchema);
+
+module.exports = Card;
