f4eed89ef807885faf39df1354905ab4f9a38c2e
tgtiburon <tg.tiburon@gmail.com>
2022-03-11 18:52:42 -0500

    finished making random dungeons

---
+++ server/models/Dungeon.js
@@ -0,0 +1,39 @@
+
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+
+const dungeonSchema = new Schema({
+  dungeonId: {
+    type: Number,
+    required: true,
+    
+  },
+  rooms: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Room",
+    }, 
+  ],
+  cards: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Card",
+    }, 
+    
+  ],
+  finalBoss: [
+      {
+          type:Schema.Types.ObjectId,
+          ref: "Enemy"
+      }
+  ]
+ 
+});
+
+
+
+const Dungeon = mongoose.model("Dungeon", dungeonSchema);
+
+module.exports = Dungeon;
