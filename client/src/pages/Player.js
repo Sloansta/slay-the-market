debde1a6cdc90d60372ade6d0c32403d69a12bcf
tgtiburon <tg.tiburon@gmail.com>
2022-03-13 19:24:09 -0400

    fixed player query

--- client/src/pages/Player.js
+++ client/src/pages/Player.js
@@ -1,44 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Nav from '../components/Nav';
-import Icon from '../components/Icon';
-import Card from '../components/Card';
+import Nav from "../components/Nav";
+import Icon from "../components/Icon";
+import Card from "../components/Card";
 
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 
-import { useQuery } from '@apollo/client';
-import { QUERY_PLAYER, QUERY_ALL_CARDS } from '../utils/queries';
+import { useQuery } from "@apollo/client";
+import { QUERY_PLAYER, QUERY_ALL_CARDS } from "../utils/queries";
 
-function Player () {
-    const { data } = useQuery(QUERY_PLAYER);
-    const loggedIn = Auth.loggedIn();
+function Player() {
+  const { data } = useQuery(QUERY_PLAYER);
+  let player;
+  if (data) {
+    player = data.player;
+    console.log("Player: ", player);
+  }
+  const loggedIn = Auth.loggedIn();
 
-    console.log(data);
-    return (
-        <section>
+ // console.log(data);
+  return (
+    <section>
+      <footer>
+        {loggedIn && (
+          <div>
+            <div>
+              <Nav />
+            </div>
 
-            <footer>
-            {loggedIn && (
+            <div>
+              <Icon />
+            </div>
 
-                <div>
-                    <div>
-                        <Nav />
-                    </div>
-
-                    <div>
-                        <Icon />
-                    </div>
-
-                    <div>
-                        <Card />
-                    </div>
-                </div>
-
-            )} 
-            </footer>
-
-        </section>
-    )
+            <div>
+              <Card />
+            </div>
+          </div>
+        )}
+      </footer>
+    </section>
+  );
 }
 
 export default Player;
