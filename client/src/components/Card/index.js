a01e1273a6b35a4db910c7374de5fad48d98ab0f
burcarz <zacharyburcar@gmail.com>
2022-03-13 13:57:18 -0400

    trying to get card animations to work

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -2,16 +2,32 @@ import React from "react";
 import { motion } from "framer-motion";
 import { UPDATE_DECK, SHUFFLE_DECK, DISCARD } from "../../utils/actions";
 function Card() {
+    const cards = [{
+        name: 'card1',
+        val: 5
+    },
+    {
+        name: 'card2',
+        val: 12
+    },
+    {
+        name: 'card3',
+        val: 8
+    }]
   return (
     <section>
-      <motion.div
-        drag="x"
-        dragConstraints={{ left: -100, right: 100 }}
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-      >
-        Cards here
-      </motion.div>
+        {cards.forEach((card) => (
+            <div>
+            <motion.div
+            drag="x"
+            dragConstraints={{ left: -100, right: 100 }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            >
+            {card.name} cards
+            </motion.div>
+            </div>
+        ))}
     </section>
   );
 }
