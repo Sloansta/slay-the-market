3f43a812c0f78ce68a988d3086570f4465df2943
brt9029 <brt9029@gmail.com>
2022-03-13 19:31:41 -0500

    added health and username rendering

--- client/src/components/Icon/index.js
+++ client/src/components/Icon/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function Icon () {
+function Icon ({username, health}) {
     
     return (
         <section>
-            <h3>Player Name: </h3>
-            <h3>Player maxHealth: </h3>
-            <h3>Player currentHealth: </h3>
+            <h3>Player Name: {username}</h3>
+            <h3>Player Health: {health}</h3>
+
         </section>
     )
 };
--- client/src/pages/Player.js
+++ client/src/pages/Player.js
@@ -4,39 +4,34 @@ import Nav from "../components/Nav";
 import Icon from "../components/Icon";
 import Card from "../components/Card";
 
-import Auth from "../utils/auth";
-
 import { useQuery } from "@apollo/client";
 import { QUERY_PLAYER, QUERY_ALL_CARDS } from "../utils/queries";
 
 function Player() {
   const { data } = useQuery(QUERY_PLAYER);
   let player;
+  let health;
   if (data) {
-    player = data.player;
-    console.log("Player: ", player);
+    player = data.player.userName;
+    health = data.player.maxHealth;
   }
-  const loggedIn = Auth.loggedIn();
-
- // console.log(data);
+  
   return (
     <section>
       <footer>
-        {loggedIn && (
           <div>
             <div>
               <Nav />
             </div>
 
             <div>
-              <Icon />
+              <Icon username={player} health={health} />
             </div>
 
             <div>
               <Card />
             </div>
           </div>
-        )}
       </footer>
     </section>
   );
