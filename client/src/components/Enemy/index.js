d0869ae14016aa246a30a43781b7cfc67799d750
tgtiburon <tg.tiburon@gmail.com>
2022-03-16 18:17:08 -0400

    temp

--- client/src/components/Enemy/index.js
+++ client/src/components/Enemy/index.js
@@ -1,59 +1,110 @@
 import React, { useEffect, useState } from "react";
-import { LOSE_HEALTH, SELECTED_ENEMY, CREATE_ENEMIES } from "../../utils/actions";
-import { stat } from "fs";
+import {
+  LOSE_HEALTH,
+  SELECTED_ENEMY,
+  CREATE_ENEMIES,
+} from "../../utils/actions";
 
 import { useGameContext } from "../../utils/GlobalState";
 import { reduceHealth } from "../../utils/helpers";
 
 function Enemy() {
   const [state, dispatch] = useGameContext();
-  // const [enemyHealth, setEnemyHealth] = useState(enemyHealth);
-  //debugger;
-  function selectedEnemy(index) {
-    let selectedEnemy = state.enemies;
 
-    // console.log(selectedEnemy);
+  function selectedEnemy(index) {
+    let selectedEnemy = state.enemies[index];
+    console.log("Selected Enemy ", selectedEnemy);
 
     if (state.selectedCard.length !== 0) {
       let enemyHealth = reduceHealth(
         state.enemies[index].currentHealth,
         state.selectedCard.value
       );
-      console.log('enemy health is now ' + enemyHealth);
- 
-      // if (state.selectedEnemy.length === 0) {
-      //   dispatch({
-      //   type: SELECTED_ENEMY,
-      //   selectedEnemy: state.enemies[index],
-      //   });
-      // }
-      // selectedEnemy[index].currentHealth = enemyHealth
+      console.log("enemy health is now " + enemyHealth);
 
-      let monsterId = state.enemies[index].name
-
-      console.log('test')
-      const newArray = selectedEnemy.map( obj => {
-        if (obj.name === monsterId) {
-          return {...obj, 
-            currentHealth: enemyHealth
-          }
-        }
-      })
-      console.log(newArray);
-
+      if (state.selectedEnemy.length === 0) {
         dispatch({
-        type: CREATE_ENEMIES,
-        enemies: newArray,
+          type: SELECTED_ENEMY,
+          selectedEnemy: state.enemies[index],
+        });
+      }
+
+      let monsterName = state.enemies[index].name;
+      console.log("Enemy name: ", monsterName);
+
+      const newArray = Array.from(state.enemies);
+      console.log("new array: ", newArray);
+      // const newArray = selectedEnemy.map((obj) => {
+      //   if (obj.name === monsterName) {
+      //     return { ...obj, currentHealth: enemyHealth };
+      //   }
+      // });
+      //  console.log(newArray);
+      // should not be needed
+      let anotherTestArray = [];
+      let i;
+      // debugger;
+      state.enemies.forEach((object) => {
+        console.log("ENEMIES", object);
+        if (selectedEnemy.name === object.name) {
+          console.log("We have a match");
+          ///selectedEnemy.health =
+          // state.setState({selectedEnemy.currentHealth : 77});
+          //  debugger;
+          // const setEnemyHealth = (enemyHealth) => {
+          //   setEnemyHealth({
+          //     ...state.enemies.currentHealth,
+          //     [selectedEnemy.currentHealth]: enemyHealth,
+          //   });
+          // };
+
+          this.setState((prevState) => ({
+            enemies: {
+              ...prevState.state.enemies,
+              currentHealth: enemyHealth,
+            },
+          }));
+        } // end of forEach
+
+        //   anotherTestArray[i] = {
+        //     __typename: "Enemy",
+        //     name: "I am a fricken test",
+        //     attack: 77,
+        //     block: 77,
+        //     currentHealth: 77,
+        //     isBoss: false,
+        //     maxHealth: 77,
+        //   };
+        i++;
       });
 
-      console.log(selectedEnemy);
+      //console.log("AnotherTest", anotherTestArray);
+      newArray.push({
+        __typename: "Enemy",
+        name: "I am a fricken test",
+        attack: 77,
+        block: 77,
+        currentHealth: 77,
+        isBoss: false,
+        maxHealth: 77,
+      });
+      // newArray[0].currentHealth = 7777;
+      // state.setState({state.enemies[index].currentHealth : 77});
+      //state.enemies[index].setState({ currentHealth: 777 });
 
+      dispatch({
+        type: CREATE_ENEMIES,
+         enemies: newArray,
+       // enemies: state.enemies,
+      });
+
+      // console.log(selectedEnemy);
     }
     if (state.enemies[index].currentHealth <= 0) {
-      document.getElementById('target').style.opacity = "25%";
+      document.getElementById("target").style.opacity = "25%";
     }
 
-    console.log(state);
+    /// console.log(state);
   }
 
   // const renderEnemies = state.enemies.map((enemy, index) =>
